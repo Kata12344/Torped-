90240ab5e22df47a092d8363a49ef252f2ce9081
ForGetASD <vitayz60@gmail.com>
2022-05-19 12:30:40 +0200

    loves metodusba dowhile ciklus létrehozása

--- Torpedo/src/torpedo/Torpedo.java
+++ Torpedo/src/torpedo/Torpedo.java
@@ -1,5 +1,7 @@
 package torpedo;
 
+import java.util.Scanner;
+
 public class Torpedo {
     static int palyahossza = 7;
     static int hajohossza = 3;
@@ -7,15 +9,19 @@ public class Torpedo {
     static int[] lovesekHelye = new int[7];
     
     public static void main(String[] args) {
-    
+        hajoELhelyez();
     }
     
     public void hajoELhelyez(){
-    
     }
     
     public void loves(){
-    
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Adj meg egy számot hogy melyik területre akarsz torpedozni: ");
+            int loves = sc.nextInt();
+        }while(true);
+        
     }
     
     public void kiiratas(){
