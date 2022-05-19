0ba9d04410957c7575cb7babdc6072ca30609901
Kata12344 <kataszundi@gmail.com>
2022-05-19 12:39:57 +0200

    hajoElhelyez metódus létrehozása

--- Torpedo/src/torpedo/Torpedo.java
+++ Torpedo/src/torpedo/Torpedo.java
@@ -1,18 +1,24 @@
 package torpedo;
 
+import java.util.Random;
+
 public class Torpedo {
     static int palyahossza = 7;
     static int hajohossza = 3;
     static int lovesekSzama = 0;
     static int[] lovesekHelye = new int[7];
-    static int[] hajoHelye = new int[3];
+    static int[] hajoHelye = new int[hajohossza];
     
     public static void main(String[] args) {
     
     }
     
-    public static void hajoELhelyez(int palyahossza, int hajohossza){
-        
+    public static void hajoELhelyez(){
+        Random rnd = new Random();
+        int hajoKezdo = rnd.nextInt(palyahossza-hajohossza)+1;
+        for (int i = 0; i < hajoHelye.length; i++) {
+            hajoHelye[i] = hajoKezdo+i;
+        }
     }
     
     public static void loves(){
@@ -20,7 +26,7 @@ public class Torpedo {
     }
     
     public static void kiiratas(){
-    
+        
     }
     
 }
