86cf34b2deea0aab3cc52c5d7eb240ecb6e0eac0
ForGetASD <vitayz60@gmail.com>
2022-05-19 12:20:05 +0200

    Globális változok deklarálása

--- Torpedo/src/torpedo/Torpedo.java
+++ Torpedo/src/torpedo/Torpedo.java
@@ -1,17 +1,25 @@
-
 package torpedo;
 
 public class Torpedo {
-
+    static int palyahossza = 7;
+    static int hajohossza = 3;
+    static int lovesekSzama = 0;
+    static int[] lovesekHelye = new int[7];
+    
     public static void main(String[] args) {
     
     }
     
     public void hajoELhelyez(){
+    
     }
     
     public void loves(){
+    
     }
+    
     public void kiiratas(){
+    
     }
+    
 }
